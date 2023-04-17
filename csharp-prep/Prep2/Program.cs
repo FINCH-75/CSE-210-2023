efcaefb81564b99456a4653ce6d784b0c7fe4299
FINCH-75 <williamchadfinch@gmail.com>
2023-04-17 16:02:29 -0700

    week 1 prep 2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -5,5 +5,52 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep2 World!");
+
+        Console.Write("What grade percentage did you recieve?");
+        string percentFromUser = Console.ReadLine() ;
+        string letterGrade = "";
+
+        int grade = int.Parse(percentFromUser);
+
+        
+
+        if (grade >= 90)
+        {
+            letterGrade = "A";
+            
+        }
+        else if (grade >=80)
+        {
+            letterGrade = "B";
+        }
+        else if (grade >=70)
+        {
+            letterGrade = "C";
+        }
+        else if (grade >=60)
+        {
+            letterGrade = "D";
+        }
+        else if (grade <= 59)
+        {
+            letterGrade = "F";
+        }
+        
+        Console.WriteLine($"Your letter grade is {letterGrade}");
+
+
+        if (grade >= 70)
+        {
+            Console.WriteLine($"Congradulations you have passed!");
+        }
+        else
+        {
+            Console.WriteLine($"Sorry you did not pass");
+        }
+        
+        
+
+        
+       
     }
 }
